0aa0d4cd4015f14d39d40f5a609f2841b0f5cac1
kanishkambk43 <kanishkambk43@gmail.com>
2026-09-14 17:53:27 +0530

    Fix Railway PostgreSQL async driver

--- app/database.py
+++ app/database.py
@@ -8,15 +8,20 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+asyncpg://",
+        1
+    )
+
 engine = create_async_engine(
     DATABASE_URL,
     echo=True
 )
 
-
-class Base(DeclarativeBase):#doubt
+class Base(DeclarativeBase):
     pass
-
 
 AsyncSessionLocal = async_sessionmaker(
     engine,
